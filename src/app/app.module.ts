50bed33567206859b82e9e47cfb21514c8474522
Julius Zeidler <jz@chaos.li>
2017-06-07 00:52:43 +0200

    Hey travis, please run?!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Geolocation } from '@ionic-native/geolocation';
+import {
+ GoogleMaps,
+ GoogleMap,
+ GoogleMapsEvent,
+ LatLng,
+ CameraPosition,
+ MarkerOptions,
+ Marker
+} from '@ionic-native/google-maps';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +35,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    GoogleMaps,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+// import {
+//  GoogleMaps,
+//  GoogleMap,
+//  GoogleMapsEvent,
+//  LatLng,
+//  CameraPosition,
+//  MarkerOptions,
+//  Marker
+// } from '@ionic-native/google-maps';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  watch: any;
+  coordinates: {};
+  map: any;
+  marker: Array<any> = [ ];
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapElement: ElementRef;
+
+
+  constructor(
+    public navCtrl: NavController,
+    private geolocation: Geolocation,
+    // private googleMaps: GoogleMaps
+  ) {
 
   }
+
+  ionViewWillEnter() {
+    this.getPosition();
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  loadMap(coords) {
+    let latLng = new google.maps.LatLng(coords.latitude, coords.longitude);
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.TERRAIN,
+      mapTypeControlStyle: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+
+  getMarker() {
+    console.log('Marker: ', this.marker);
+  }
+
+  addMarker() {
+    this.getMarker();
+    this.marker.push(new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter(),
+      clickable: true
+    }));
+
+
+  }
+
+  updatePosition(coords) {
+    let latLng = new google.maps.LatLng(coords.latitude, coords.longitude);
+    this.map.setCenter(latLng);
+  }
+  getPosition () {
+    this.geolocation.getCurrentPosition().then(
+      (res) => {
+        console.log(res.coords);
+        this.coordinates = res.coords;
+        this.loadMap(res.coords);
+      }
+    ).catch(
+      (err) => {
+        console.log('Error getting location ', err);
+        alert('Error getting location');
+      }
+    )
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe(
+      (data) => {
+        console.log(data.coords);
+        this.updatePosition(data.coords);
+      }
+    )
+
+  }
+
+
+
 
 }
